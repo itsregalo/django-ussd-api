3f99544ef2e02fa2eaa10aa4f9cd28ea388165f8
retech <itsregalo047@gmail.com>
2022-03-18 11:51:31 +0300

    heroku configs

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-w^mw#d+*nr&o-&_6yvqz-c%4hz^bpnb7b$vtjl2ut)4)+1w%+&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    'django-innovation-ussd.herokuapp.com',
+    '127.0.0.1:8000'
+]
 
 
 # Application definition
@@ -42,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,6 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+
+    BASE_DIR / 'static',
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
